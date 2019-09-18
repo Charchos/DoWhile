94506ce4a66029830470da2c76ae2fdc685f342c
Alumno <Alumno@PC26>
2019-09-18 11:35:12 -0500

    dowhile2

---
+++ dowhile2/dowhile2/dowhile2.cpp
@@ -0,0 +1,23 @@
+
+// dowhile2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	int opcion = 0;
+	do
+	{
+		cout << "Ingrese 1 para mostrar mensaje\n ";
+		cout << "Ingrese 2 para mostrar algo mas\n ";
+		cout << "Ingrese 0 para salir\n ";
+		cout << "Ingrese una opcion: \n ";
+		cin >> opcion;
+	} while (opcion != 0);
+
+	return 0;
+}
